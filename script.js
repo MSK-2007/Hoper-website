78ed2d4c1414c8448d4be1b50ecf8cdb50c3729f
Manish <manishshivakumar178@gmail.com>
2025-10-14 18:34:54 +0530

    Fix invalid API key

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAN2oL3U1gMYOn7-WqgnEE719yaLoii8jA",
   authDomain: "hoper-cf7e8.firebaseapp.com",
   projectId: "hoper-cf7e8",
-  storageBucket: "hoper-cf7e8.appspot.com",
+  storageBucket: "hoper-cf7e8.firebasestorage.app",
   messagingSenderId: "873807430355",
   appId: "1:873807430355:web:a60afb7d06f884f80f0a26",
   measurementId: "G-9VCJ6GM2EX"
 };
-
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
